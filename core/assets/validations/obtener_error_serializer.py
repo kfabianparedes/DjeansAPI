97971c55c01ea26a80f50db1960b4045f2cdb201
Kevin Hernán Fabián Paredes <kfabianparedes@gmail.com>
2022-03-14 18:35:48 -0500

    validar espacios y caracteres alfabéticos (nueva función en validaciones)

--- core/assets/validations/obtener_error_serializer.py
+++ core/assets/validations/obtener_error_serializer.py
@@ -1,3 +1,6 @@
+import re
+
+
 def obtenerErrorSerializer(value):
     return list(value.errors.values())[0][0]
 
@@ -12,4 +15,10 @@ def validarEsMayorQueCero(value):
     if validarEsNumerico(value):
         if int(value) > 0:
             return True
+    return False
+
+
+def validarCaracteresAlfabeticoConEspacios(value):
+    if re.match(r"[a-zA-Z ]+$", str(value)):
+        return True
     return False
